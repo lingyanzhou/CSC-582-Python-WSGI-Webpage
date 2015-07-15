9175cea4c5b57322fa7cf3046a35f6d628029f49
zhoulingyan <zhoulingyan@zhoulingyan-pc.zhoulingyan-pc>
2015-07-15 06:42:28 -0700

    deletion

--- projectkernel/command.py
+++
@@ -1,7 +0,0 @@
-class Command:
-    def __init__(self):
-        pass
-    def run(self):
-        pass
-    def get_name(self):
-        pass
--- projectkernel/dataaccess.py
+++
@@ -1,7 +0,0 @@
-class dataaccess:
-    def __init__(self):
-        pass
-    def save(self):
-        pass
-    def load(self):
-        pass
